af2f93b4661e64de2988635e1b4a6c1598d0b984
Sven <svo@otobo.org>
2020-07-11 18:07:37 +0200

    Add package version to packagesetup.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -74,7 +74,7 @@ sub new {
     ];
 
     # define the version of the included packages TODO
-    $Self->{PackageVersion} = '10.0.0';
+    $Self->{PackageVersion} = '10.0.1';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
