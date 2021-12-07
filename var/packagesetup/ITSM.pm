7b558a18e2f279510e207c4b88ca9e45039382ac
Sven <sven.oesterling@protonmail.com>
2021-12-07 12:46:19 +0100

    Update version info to 10.1.1.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -74,7 +74,7 @@ sub new {
     ];
 
     # define the version of the included packages TODO
-    $Self->{PackageVersion} = '10.0.3';
+    $Self->{PackageVersion} = '10.1.1';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
